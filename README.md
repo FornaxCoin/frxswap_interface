946c6cd2f541fb1928d20ec4ed8de9fbc0695a77
qasim raheem <qasimmehmood13936@gmail.com>
2022-07-25 03:53:34 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Uniswap-UI-fork template
+
+### Rinkeby
+
+- Factory: 0xa8f407b929b0Ad52dEa54cCa07ae5101ded0C4f1
+- Router: 0x1DDdd59CBaF43e40f6E08c73795d7B81B838DAE2
+- Pair hash: 0xf5a445fc2dcbf55ed638e7001307205d27c25f2efe96442d5fda4feeca06d78e
---
+++ node_modules/@uniswap/sdk/sdk/dist/constants.d.ts
@@ -0,0 +1,39 @@
+import JSBI from 'jsbi';
+export declare type BigintIsh = JSBI | bigint | string;
+export declare enum ChainId {
+    MAINNET = 1,
+    ROPSTEN = 3,
+    RINKEBY = 4,
+    FORNAX = 13937,
+    GÖRLI = 5,
+    KOVAN = 42
+}
+export declare enum TradeType {
+    EXACT_INPUT = 0,
+    EXACT_OUTPUT = 1
+}
+export declare enum Rounding {
+    ROUND_DOWN = 0,
+    ROUND_HALF_UP = 1,
+    ROUND_UP = 2
+}
+export declare const FACTORY_ADDRESS = "0xafD0c4Ad200982b17092768f4df60be3b8169D83";
+export declare const INIT_CODE_HASH = "0xf5a445fc2dcbf55ed638e7001307205d27c25f2efe96442d5fda4feeca06d78e";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const TWO: JSBI;
+export declare const THREE: JSBI;
+export declare const FIVE: JSBI;
+export declare const TEN: JSBI;
+export declare const _100: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
+export declare enum SolidityType {
+    uint8 = "uint8",
+    uint256 = "uint256"
+}
+export declare const SOLIDITY_TYPE_MAXIMA: {
+    uint8: JSBI;
+    uint256: JSBI;
+};
